68f520b71e3c81d4e39852f9af324fc7822835ce
Buddy <buddy.gorven@aotal.com>
2017-09-18 19:14:16 +1200

    Use after_success script instead of deployment provider

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker build -t aotallabs/frisket:"$TRAVIS_BUILD_NUMBER" .;
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD";
-docker push aotallabs/frisket:"$TRAVIS_BUILD_NUMBER";
